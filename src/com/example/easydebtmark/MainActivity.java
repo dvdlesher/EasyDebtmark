b402e7687cd4baf217f4181c1fba471abb210d4f
dvdlesher <dvdlesher@gmail.com>
2014-05-27 18:50:32 +1000

    Voice input is now parsed at bare minimum level.
    The only pattern the code can parse is "[name] [amount] for [details].
    This will be upgraded after other section is done.
    
    Next target is implementing database.

--- src/com/example/easydebtmark/MainActivity.java
+++ src/com/example/easydebtmark/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.easydebtmark;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -26,6 +32,7 @@ public class MainActivity extends Activity {
 	private UnspentMoneyDatabase umDB;
 	private List<DebtPerson> mainlist;
 	private List<String> stringRelatedToNumber;
+	private List<String> alertBuilderOptions;
 
 	/**
 	 * Called with the activity is first created.
@@ -35,40 +42,37 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainpage);
 		/*
-		stringRelatedToNumber.add("one");
-		stringRelatedToNumber.add("two");
-		stringRelatedToNumber.add("three");
-		stringRelatedToNumber.add("four");
-		stringRelatedToNumber.add("five");
-		stringRelatedToNumber.add("six");
-		stringRelatedToNumber.add("seven");
-		stringRelatedToNumber.add("eight");
-		stringRelatedToNumber.add("nine");
-		stringRelatedToNumber.add("ten");
-		stringRelatedToNumber.add("eleven");
-		stringRelatedToNumber.add("twelve");
-		stringRelatedToNumber.add("thirteen");
-		stringRelatedToNumber.add("fourteen");
-		stringRelatedToNumber.add("fifteen");
-		stringRelatedToNumber.add("sixteen");
-		stringRelatedToNumber.add("seventeen");
-		stringRelatedToNumber.add("eighteen");
-		stringRelatedToNumber.add("nineteen");
-		stringRelatedToNumber.add("twenty");
-		stringRelatedToNumber.add("thirty");
-		stringRelatedToNumber.add("forty");
-		stringRelatedToNumber.add("fifty");
-		stringRelatedToNumber.add("sixty");
-		stringRelatedToNumber.add("seventy");
-		stringRelatedToNumber.add("eighty");
-		stringRelatedToNumber.add("ninty");
-		stringRelatedToNumber.add("hundred");
-		stringRelatedToNumber.add("thousand");
-		stringRelatedToNumber.add("million");
-		stringRelatedToNumber.add("billion");
-		stringRelatedToNumber.add("trillion");
-		*/
+		 * stringRelatedToNumber.add("one"); stringRelatedToNumber.add("two");
+		 * stringRelatedToNumber.add("three");
+		 * stringRelatedToNumber.add("four"); stringRelatedToNumber.add("five");
+		 * stringRelatedToNumber.add("six"); stringRelatedToNumber.add("seven");
+		 * stringRelatedToNumber.add("eight");
+		 * stringRelatedToNumber.add("nine"); stringRelatedToNumber.add("ten");
+		 * stringRelatedToNumber.add("eleven");
+		 * stringRelatedToNumber.add("twelve");
+		 * stringRelatedToNumber.add("thirteen");
+		 * stringRelatedToNumber.add("fourteen");
+		 * stringRelatedToNumber.add("fifteen");
+		 * stringRelatedToNumber.add("sixteen");
+		 * stringRelatedToNumber.add("seventeen");
+		 * stringRelatedToNumber.add("eighteen");
+		 * stringRelatedToNumber.add("nineteen");
+		 * stringRelatedToNumber.add("twenty");
+		 * stringRelatedToNumber.add("thirty");
+		 * stringRelatedToNumber.add("forty");
+		 * stringRelatedToNumber.add("fifty");
+		 * stringRelatedToNumber.add("sixty");
+		 * stringRelatedToNumber.add("seventy");
+		 * stringRelatedToNumber.add("eighty");
+		 * stringRelatedToNumber.add("ninty");
+		 * stringRelatedToNumber.add("hundred");
+		 * stringRelatedToNumber.add("thousand");
+		 * stringRelatedToNumber.add("million");
+		 * stringRelatedToNumber.add("billion");
+		 * stringRelatedToNumber.add("trillion");
+		 */
 		openDatabase();
+		alertBuilderOptions = new ArrayList<String>();
 
 		Button speakButton = (Button) findViewById(R.id.speakButton);
 
@@ -86,7 +90,10 @@ public class MainActivity extends Activity {
 	private void openDatabase() {
 		debtDB = new DebtDatabase(this);
 		umDB = new UnspentMoneyDatabase(this);
-
+		
+		//TODO: Do these in separate thread. Thanks Android.
+		debtDB.open();
+		umDB.open();
 	}
 
 	private void initializeMainList() {
@@ -145,19 +152,145 @@ public class MainActivity extends Activity {
 			// engine thought it heard
 			ArrayList<String> matches = data
 					.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-			// TODO: Popup a list of possible options then let user decide (FOR NOW IT ALWAYS CHOOSE THE FIRST ONE)
-			String selectedString = matches.get(0);
-			Toast.makeText(this, selectedString, Toast.LENGTH_LONG).show();
+			AlertDialog.Builder options = new AlertDialog.Builder(this);
+			options.setTitle("");
 
-			// TODO: PARSE the selected string (NAME AMOUNT DETAILS)
-			String[] section = selectedString.split(" ");
-			String name;
-			float moneyOwed;
-			String details;
+			alertBuilderOptions.clear();
+			CharSequence results[] = new CharSequence[matches.size()];
+			for (int i = 0; i < matches.size(); i++) {
+				results[i] = (CharSequence) matches.get(i);
+				alertBuilderOptions.add(matches.get(i));
+			}
 
-			String date;
-			// TODO: Then input it to both database (new entry for debtDB,
-			// update for umDB)
+			options.setItems(results, new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// String selectedString = matches.get(which);
+					// Toast.makeText(this, selectedString,
+					// Toast.LENGTH_LONG).show();
+
+					// String[] section = selectedString.split(" ");
+
+					// FORMATS:
+					// I owe/borrowed [name] [amount] for [details]
+					// I owe/borrowed [amount] to [name] for [details]
+					// [name] lend/lent (me) [amount] for [details]
+
+					// String[] dataArray =
+					// alertBuilderOptions.get(which).toLowerCase().split(" ");
+					String[] dataArray = "Some shitty dude 150 dollars 30 cents for piece of crappy crap rap"
+							.split(" ");
+
+					// int indexName = 0;
+					int indexMoney = 99;
+					float indexMoneyCent = 1;
+					int indexDetails = 99;
+
+					String name = "";
+					float moneyOwed = 0;
+					String details = "";
+
+					// if (dataArray[0].startsWith("i")) {
+					// for (int i = 2; i < dataArray.length; i++) {
+					// if (TextUtils
+					// .isDigitsOnly((CharSequence) dataArray[i])) {
+					// moneyOwed += Float.parseFloat(dataArray[i])
+					// / indexMoneyCent;
+					// indexMoneyCent = 100;
+					// } else if (dataArray[i].startsWith("for")) {
+					// indexDetails = i + 1;
+					// } else if (dataArray[i].startsWith("cent")
+					// || dataArray[i].startsWith("dollar")
+					// || dataArray[i].startsWith("to")
+					// || i >= indexDetails - 1) {
+					// // DO NOTHING
+					// } else {
+					// name.concat(dataArray[i]);
+					// }
+					// }
+					// for (int i = indexDetails; i < dataArray.length; i++) {
+					// details.concat(dataArray[i]);
+					// }
+					// } else {
+					// indexName = 0;
+					// for (int i = 1; i < dataArray.length; i++) {
+					// if (dataArray[i].startsWith("lend")
+					// || dataArray[i].startsWith("lent")) {
+					// indexMoney = i + 1;
+					// } else if (dataArray[i].startsWith("for")) {
+					// indexDetails = i + 1;
+					// }
+					// }
+					//
+					// for (int i = 0; i < dataArray.length; i++) {
+					// if (i < indexMoney - 1) {
+					// name.concat(dataArray[i]);
+					// } else if (i == indexMoney - 1) {
+					// // NOPE
+					// } else if (i < indexDetails - 1) {
+					// moneyOwed += Float.parseFloat(dataArray[i])
+					// / indexMoneyCent;
+					// indexMoneyCent = 100;
+					// i++;
+					// } else if (i == indexDetails - 1) {
+					// // NOPE
+					// } else {
+					// details.concat(dataArray[i]);
+					// }
+					// }
+					// }
+
+					// Fuck that format, just go with "[name] [money] for
+					// [details]"
+
+					for (int i = 0; i < dataArray.length; i++) {
+						if (TextUtils.isDigitsOnly((CharSequence) dataArray[i])
+								&& indexMoney >= i) {
+							indexMoney = i;
+						} else if (dataArray[i].startsWith("for")) {
+							i++;
+							indexDetails = i;
+						} else {
+							// Nothing
+						}
+					}
+
+					for (int i = 0; i < dataArray.length; i++) {
+						if (i < indexMoney) {
+							name += dataArray[i];
+						} else if (i < indexDetails - 1
+								&& (i + 1 < dataArray.length)
+								&& (dataArray[i + 1].startsWith("dollar"))) {
+							moneyOwed += Float.parseFloat(dataArray[i]);
+							i++;
+						} else if (i < indexDetails - 1
+								&& TextUtils
+										.isDigitsOnly((CharSequence) dataArray[i])) {
+							moneyOwed += Float.parseFloat(dataArray[i]) / 100;
+						} else if (i == indexDetails - 1) {
+							// NOPE, skip "for" text
+						} else if (i >= indexDetails) {
+							details += dataArray[i];
+						} else {
+							// DO NOTHING
+						}
+					}
+
+					DateFormat dateFormat = new SimpleDateFormat(
+							"yyyy/MM/dd HH:mm:ss");
+					Calendar cal = Calendar.getInstance();
+					String date = dateFormat.format(cal.getTime());
+
+					// TODO: Then input it to both database (new entry for
+					// debtDB,
+					// update for umDB)
+					debtDB.insertRow(name, date, moneyOwed, 0, details);
+					
+
+				}
+			});
+			options.show();
 
 		}
 		super.onActivityResult(requestCode, resultCode, data);
